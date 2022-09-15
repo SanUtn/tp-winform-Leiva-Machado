6ad8dc721db0006801b6a49102ba8048b332f3db
Cristian Machado <56597872+cmachado17@users.noreply.github.com>
2022-09-15 12:20:27 -0300

    ver detalle: ajuste visual y ventana de dialogo

--- TPWinForm_Leiva_Machado/TPWinForm/Detalle.Designer.cs
+++ TPWinForm_Leiva_Machado/TPWinForm/Detalle.Designer.cs
@@ -41,17 +41,19 @@ namespace TPWinForm
             // 
             this.lbTitulo.AutoSize = true;
             this.lbTitulo.BackColor = System.Drawing.Color.Transparent;
-            this.lbTitulo.Location = new System.Drawing.Point(13, 13);
+            this.lbTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(31, 26);
             this.lbTitulo.Name = "lbTitulo";
-            this.lbTitulo.Size = new System.Drawing.Size(0, 13);
+            this.lbTitulo.Size = new System.Drawing.Size(0, 25);
             this.lbTitulo.TabIndex = 0;
             // 
             // pbDetalle
             // 
             this.pbDetalle.BackColor = System.Drawing.Color.Transparent;
-            this.pbDetalle.Location = new System.Drawing.Point(141, 276);
+            this.pbDetalle.Location = new System.Drawing.Point(94, 190);
             this.pbDetalle.Name = "pbDetalle";
-            this.pbDetalle.Size = new System.Drawing.Size(100, 95);
+            this.pbDetalle.Size = new System.Drawing.Size(178, 176);
+            this.pbDetalle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbDetalle.TabIndex = 1;
             this.pbDetalle.TabStop = false;
             // 
@@ -59,27 +61,30 @@ namespace TPWinForm
             // 
             this.lbNombre.AutoSize = true;
             this.lbNombre.BackColor = System.Drawing.Color.Transparent;
-            this.lbNombre.Location = new System.Drawing.Point(16, 37);
+            this.lbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.Location = new System.Drawing.Point(31, 63);
             this.lbNombre.Name = "lbNombre";
-            this.lbNombre.Size = new System.Drawing.Size(0, 13);
+            this.lbNombre.Size = new System.Drawing.Size(0, 25);
             this.lbNombre.TabIndex = 2;
             // 
             // lbDescripcion
             // 
             this.lbDescripcion.AutoSize = true;
             this.lbDescripcion.BackColor = System.Drawing.Color.Transparent;
-            this.lbDescripcion.Location = new System.Drawing.Point(16, 72);
+            this.lbDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescripcion.Location = new System.Drawing.Point(31, 100);
             this.lbDescripcion.Name = "lbDescripcion";
-            this.lbDescripcion.Size = new System.Drawing.Size(0, 13);
+            this.lbDescripcion.Size = new System.Drawing.Size(0, 25);
             this.lbDescripcion.TabIndex = 3;
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
             this.lblPrecio.BackColor = System.Drawing.Color.Transparent;
-            this.lblPrecio.Location = new System.Drawing.Point(19, 126);
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(31, 137);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(13, 13);
+            this.lblPrecio.Size = new System.Drawing.Size(25, 25);
             this.lblPrecio.TabIndex = 4;
             this.lblPrecio.Text = "$";
             // 
@@ -93,7 +98,10 @@ namespace TPWinForm
             this.Controls.Add(this.lbNombre);
             this.Controls.Add(this.pbDetalle);
             this.Controls.Add(this.lbTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Detalle";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Detalle";
             ((System.ComponentModel.ISupportInitialize)(this.pbDetalle)).EndInit();
             this.ResumeLayout(false);
