8d50975c0bb138d6179575e4fed2a37a36d0637d
SanUtn <sandra.leiva@alumnos.frgp.utn.edu.ar>
2022-09-18 20:23:54 -0300

    home: update

--- TPWinForm_Leiva_Machado/TPWinForm/Home.Designer.cs
+++ TPWinForm_Leiva_Machado/TPWinForm/Home.Designer.cs
@@ -30,32 +30,47 @@ namespace TPWinForm
         private void InitializeComponent()
         {
             this.lbHome = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbHome
             // 
             this.lbHome.AutoSize = true;
+            this.lbHome.BackColor = System.Drawing.Color.Transparent;
+            this.lbHome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lbHome.Font = new System.Drawing.Font("Segoe UI Symbol", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbHome.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.lbHome.Location = new System.Drawing.Point(241, 175);
+            this.lbHome.Location = new System.Drawing.Point(253, 200);
             this.lbHome.Name = "lbHome";
-            this.lbHome.Size = new System.Drawing.Size(339, 65);
+            this.lbHome.Size = new System.Drawing.Size(341, 67);
             this.lbHome.TabIndex = 0;
             this.lbHome.Text = "BIENVENIDOS";
             this.lbHome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TPWinForm.Properties.Resources.imagenEcomerce;
+            this.pictureBox1.Location = new System.Drawing.Point(-53, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(928, 489);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(797, 451);
+            this.ClientSize = new System.Drawing.Size(865, 474);
             this.Controls.Add(this.lbHome);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Home";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
             this.TransparencyKey = System.Drawing.Color.Red;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -64,5 +79,6 @@ namespace TPWinForm
         #endregion
 
         private System.Windows.Forms.Label lbHome;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
